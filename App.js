1d63fbb34906e78419de92688de60d283d014d07
Alexis Felipe Elorza Obregon <142292650+Elorza4A@users.noreply.github.com>
2025-10-24 21:48:29 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.blanco}>Mi primera aplicacion movil</Text>
+      <Text style={styles.blanco}>Hola mundo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  blanco: {
+    color: '#fff',
+  },
+});
